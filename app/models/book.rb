5a79fb3dd77d31d8702112b1b2cf8290dd4a7855
Alec Wells <alec.wells47@gmail.com>
2019-08-30 11:57:48 -0600

    Change validation to numericality in book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,10 @@
 class Book < ApplicationRecord
   has_many :author_books
   has_many :authors, through: :author_books
-  validates_presence_of :title, :page_count, :publication_year
+  validates_presence_of :title
+
+  validates :page_count, numericality: { only_integer: true,
+                                        greater_than_or_equal_to: 1}
+
+  validates :publication_year, numericality: { only_integer: true}
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe Book, type: :model do
   describe "validations" do
     it { should validate_presence_of :title}
-    it { should validate_presence_of :page_count}
-    it { should validate_presence_of :publication_year}
+    it { should validate_numericality_of(:page_count) }
+    it { should validate_numericality_of :publication_year}
   end
 
   describe "relationships" do

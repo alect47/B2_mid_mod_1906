a58be7fbee5775be7b1d3fa2269de53235f7805b
Alec Wells <alec.wells47@gmail.com>
2019-09-02 22:10:17 -0600

    Add model test for average_pages)

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,4 +9,19 @@ describe Author, type: :model do
     it { should have_many(:books).through(:author_books)}
     it { should have_many(:author_books)}
   end
+
+  describe "instance methods" do
+    before(:each) do
+      @stephen = Author.create(name: "Stephen King")
+      @stand = Book.create(title: "The Stand", page_count: 740, publication_year: 1994 )
+      @gunslinger = Book.create(title: "The Gunslinger", page_count: 840, publication_year: 1996 )
+
+      @stephen.books << @stand
+      @stephen.books << @gunslinger
+    end
+
+    it "can calculate average pages" do
+      expect(@stephen.average_pages).to eq(790)
+    end
+  end
 end
